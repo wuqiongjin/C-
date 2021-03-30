e07b4eea93acbd66b2cfb999dfb164cdf41fe9d2
wuqiongjin <1301492906@qq.com>
2021-03-30 09:38:31 +0800

    test

---
+++ C++基础/test.cpp
@@ -0,0 +1,20 @@
+//#include <iostream>
+////using namespace std;
+//
+//namespace N
+//{
+//	int a = 10;
+//	double b = 1.1;
+//}
+//
+//using namespace N;
+//using namespace std;
+//int main()
+//{
+//
+//	
+//	cout << "hello word!!!" << endl;
+//	
+//	cout << a    <<std::endl<<b <<endl;
+//	return 0;
+//}
