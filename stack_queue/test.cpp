70b3068ac0cc159523b825320ee52a4da1ace13f
wuqiongjin <1301492906@qq.com>
2021-07-04 23:52:46 +0800

    update some tests

--- stack_queue/test.cpp
+++ stack_queue/test.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
+#include <assert.h>
 using namespace std;
 #include <vector>
 #include <list>
 #include <deque>
+#include <queue>		//priority_queue在里面
+#include <functional>
 #include "stack.h"
 #include "queue.h"
-
+#include "priority_queue.h"
 
 //#include <stack>
 //#include <queue>
@@ -69,6 +72,61 @@ void testdeque()
 	//cout << endl;
 }
 
+void test_priority_queue()
+{
+	vector<int> v{ 1,2,3,4,5 };
+	int arr[] = { -1,-2,-3,-4,-5 };
+
+	////cout << v << endl;				//v是个对象。。。
+	//cout << &v << endl;			
+	////cout << v.begin() << endl;		//cout没有重载vector迭代器的输出
+	//cout << typeid(arr).name() << endl;
+
+	//cout << (v.begin() + 5 == v.end()) << endl;
+
+	
+	//priority_queue<int> pq;
+	priority_queue<int, vector<int>, greater<int>> pq(v.begin(), v.begin()+5);	//priority_queue可以给迭代器的范围以用来初始化
+
+	//pq.push(1);
+	//pq.push(-10);
+	//pq.push(54);
+	//pq.push(32);
+	//pq.push(39);
+
+	while (!pq.empty())
+	{
+		cout << pq.top() << " ";
+		pq.pop();
+	}
+	cout << endl;
+}
+
+void test_sort()
+{
+	vector<int> v;
+	v.push_back(1);
+	v.push_back(22);
+	v.push_back(9);
+	v.push_back(4);
+	v.push_back(8);
+
+	//sort中传的是对象
+
+	//默认排 升序	less <
+	sort(v.begin(), v.end());
+
+	//	  排 降序	greater >
+	sort(v.begin(), v.end(), greater<int>());	//使用匿名对象
+
+	for (auto e : v)
+	{
+		cout << e << " ";
+	}
+	cout << endl;
+}
+
+
 int main()
 {
 	//test_stack();
@@ -76,5 +134,9 @@ int main()
 
 	//wqj::teststack();
 	//wqj::testqueue();
-	testdeque();
+	//testdeque();
+	//test_priority_queue();
+	//wqj::test_priority_queue();
+
+	test_sort();
 }
