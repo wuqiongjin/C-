cb96132fff93758d8428590735493c0a35e5412b
wuqiongjin <1301492906@qq.com>
2021-08-01 22:11:38 +0800

    update info about blogs

--- 模板进阶/test.cpp
+++ 模板进阶/test.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
 using namespace std;
 
+//template <class T>
+//class Stack
+//{
+//public:
+//	Stack() {};
+//	void push_back(const T& x);
+//private:
+//	T*	   _a;
+//	size_t _capacity;
+//	size_t _size;
+//};
+//
+//template <class T>
+//void Stack<T>::push_back(const T& x)
+//{}
+
+
+
+
 //非类型模板参数
 template <class T, int N>
 class Array
@@ -18,17 +37,39 @@ private:
 
 
 //函数模板的特化
-template <class T1, class T2>
-T1 Add(T1 x, T2 y)
-{
-	return x + y;
-}
+//template <class T>
+//bool isEqual(T x1, T x2)
+//{
+//	return x1 == x2;
+//}
+//
+//template <>
+//bool isEqual<char*>(char* str1, char* str2)
+//{
+//	return !strcmp(str1, str2);
+//}
 
-template <>
-char Add<char, char>(char a, char b)
-{
-	return a + b;
-}
+
+//template <class T>
+//bool isEqual(const T& x1, const T& x2)
+//{
+//	return x1 == x2;
+//}
+//
+////错误，函数参数列表的类型 与 原模板的参数列表的基础类型不相同
+//template <>
+//bool isEqual<const char*>(const char* str1, const char* str2)
+//{
+//	return !strcmp(str1, str2);
+//}
+//
+////正确
+//template <>
+//bool isEqual<const char*>(const char* const& str1, const char* const& str2)
+//{
+//	return !strcmp(str1, str2);
+//}
+
 
 
 //类模板的特化
@@ -90,7 +131,6 @@ private:
 	T2* _b;
 };
 
-
 int main()
 {
 	//Array<int, 10> a;
@@ -100,6 +140,37 @@ int main()
 	//A<int, int>  a2;
 	//A<int*, int*> a3;
 	//A<char*, char*> a4;
-	Add<int, int>(1, 3);
+	//Add<int, int>(1, 3);
+	//Stack<int> st1;
+	//Stack<double> st2;
+
 	return 0;
 }
+
+//博客内容
+
+////"A.h"
+//template <class T>
+//bool isEqual(const T& x1, const T& x2);
+//
+////A.cpp
+//#include "A.h"
+//template <class T>
+//bool isEqual(const T& x1, const T& x2)
+//{
+//	return x1 == x2;
+//}
+//
+//template
+//bool isEqual<int>(const int& x1, const int& x2);
+//
+//template
+//bool isEqual<double>(const double& x1, const double& x2);
+//
+////main.cpp
+//int main()
+//{
+//	isEqual<int>(1, 2);
+//
+//	return 0;
+//}
